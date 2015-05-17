0033651dcecb28b090ecdc4c98cae778dabab61e
Nick Beeuwsaert <nick.beeuwsaert@gmail.com>
2015-05-17 17:55:02 -0500

    Added documentation, test designs, all that y'know....

---
+++ README.rst
@@ -0,0 +1,12 @@
+==================
+AFDesign Extractor
+==================
+NOTE: This project is a work-in-progress.
+
+Currently the extracting resources from a afdesign is working. but parsing the data further than that hasn't been accomplished yet.
+
+Purpose
+=======
+It'd be useful to have a command line tool / library that will extract resources from a afdesign and convert each slice to a SVG/PNG/JPG/Whatever. 
+
+Also: I like mucking around in file-formats.
--- main.py
+++ main.py
@@ -5,17 +5,6 @@ from datetime import datetime
 from collections import namedtuple
 import os.path
 
-unpack = lambda f, fmt: struct.unpack(fmt, f.read(struct.calcsize(fmt)))
-
-Header = namedtuple(
-    'AFDesignHeader',
-    (
-        'signature',
-        'version',
-        'inexplicably_prsn'
-    )
-)
-
 from affinity import AffinityFile
 
 if __name__ == "__main__":
@@ -38,41 +27,3 @@ if __name__ == "__main__":
         print("Writing {}...".format(outfile))
         with open(outfile, "wb+") as output:
             output.write(f.data)
-
-#    header = Header(*unpack(inp, "<II4s"))
-#
-#    print("""
-#        Signature:         {h.signature:08x}
-#        Version:           {h.version}
-#        Inexplicably Prsn: {h.inexplicably_prsn}
-#    """.format(h=header))
-#
-#    offsets = []
-#    chunk, = unpack(inp, "<4s")
-#    assert(chunk == "#Inf")
-#
-#    fat_offset, fat_length, maybe_fil_length, zero = unpack(inp, "<4Q");
-#    print("""
-#            FAT at: {},
-#            Length: {},
-#            As of yet, still undiscovered offset: {}
-#            usually zero: {}
-#            """.format(fat_offset, fat_length - fat_offset, maybe_fil_length, zero))
-#    timestamp, = unpack(inp, "<I")
-#    print("Creation date: {:%Y-%m-%d %H:%M:%S}".format(datetime.fromtimestamp(timestamp)))
-#    print("Creation date: {}".format(datetime.fromtimestamp(timestamp).isoformat()))
-#
-#
-#    inp.seek(fat_offset)
-#    fat = inp.read(fat_length - fat_offset)
-#    print("Read FAT!");
-#
-#    #if unpack(inp, '<4s') == ('#Inf',):
-#    #    print("Reading offsets...")
-#    #    while True:
-#    #        offset, = unpack(inp, "<Q")
-#    #        print(offset)
-#    #        if offset == 0: break
-#    #        offsets += [offset]
-#
-#    #print(offsets)
